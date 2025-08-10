da97c92e61e81914b3a0657867d0994c6fb35ff3
Csiszár Attila <csiszar.ati@gmail.com>
2025-08-10 15:50:13 +0000

    Start using neovim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,23 @@
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+vim.opt.signcolumn = 'yes'
+
+vim.api.nvim_create_autocmd("Filetype", {
+  pattern = "make",
+  callback = function()
+    vim.opt_local.expandtab = false 
+  end,
+})
+
+vim.diagnostic.config({
+    virtual_text = true,
+})
+
+vim.pack.add({
+    { src = 'https://github.com/neovim/nvim-lspconfig' },
+})
+
+vim.lsp.enable('clangd')
