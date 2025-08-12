8cc872773bb76ab5862dbb7ae14ffa6a1627d8af
Csiszár Attila <csiszar.ati@gmail.com>
2025-08-12 19:09:06 +0000

    No more how to quit from vim ;)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,6 +5,10 @@ vim.opt.expandtab = true
 vim.opt.encoding = "utf-8"
 vim.opt.fileencoding = "utf-8"
 vim.opt.signcolumn = 'yes'
+vim.g.mapleader = " "
+
+vim.keymap.set('n', '<leader>w', ':write<CR>')
+vim.keymap.set('n', '<leader>q', ':quit<CR>')
 
 vim.api.nvim_create_autocmd("Filetype", {
   pattern = "make",
